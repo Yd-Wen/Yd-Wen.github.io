6f323a83e62bbb1f38a3bdc8e779c92e2e78ee4a
Yindong Wen <wenyindong@163.com>
2026-03-01 16:26:30 +0800

    update navbar item: note-ai

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -11,6 +11,7 @@ export const zhNavbarConfig = navbar([
       "big_data/",          // 大数据
       "machine_learning/",  // 机器学习
       "deep_learning/",     // 深度学习
+      "ai/",                // AI 应用开发
       "other/",             // 其他
     ],
   },
