2a2da0cd0155a803506cd7ad4517d2bda86ef060
Yindong Wen <wenyindong@163.com>
2026-03-14 16:06:19 +0800

    update icons

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -107,7 +107,7 @@ export default hopeTheme(
       },
 
       icon: {
-        assets: "//at.alicdn.com/t/c/font_5129685_03p4ooge84cq.css",
+        assets: "//at.alicdn.com/t/c/font_5129685_yyisjupc77r.css",
       },
 
       pwa: {
